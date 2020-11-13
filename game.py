6e4b95f1b25063291c909ad539fd8cfe24986b39
Yash Vadhadiya <61732752+YashVadhadiya@users.noreply.github.com>
2020-11-13 22:03:48 +0530

    Update game.py

--- game.py
+++ game.py
@@ -25,3 +25,70 @@ snake_head = (247, 220, 111)
 
 
 # ----------- Snake Class ----------------
+class Snake:
+    def __init__(self, x_start, y_start):
+        self.x = x_start
+        self.y = y_start
+        self.w = 10
+        self.h = 10
+        self.x_dir = 1
+        self.y_dir = 0
+        self.history = [[self.x, self.y]]
+        self.length = 1
+
+    def reset(self):
+        self.x = width/2-scale
+        self.y = height/2-scale
+        self.w = 10
+        self.h = 10
+        self.x_dir = 1
+        self.y_dir = 0
+        self.history = [[self.x, self.y]]
+        self.length = 1
+
+    def show(self):
+        for i in range(self.length):
+            if not i == 0:
+                pygame.draw.rect(display, snake_colour, (self.history[i][0], self.history[i][1], self.w, self.h))
+            else:
+                pygame.draw.rect(display, snake_head, (self.history[i][0], self.history[i][1], self.w, self.h))
+
+    def check_eaten(self):
+        if abs(self.history[0][0] - food_x) < scale and abs(self.history[0][1] - food_y) < scale:
+            return True
+
+    def grow(self):
+        self.length += 1
+        self.history.append(self.history[self.length-2])
+
+    def death(self):
+        i = self.length - 1
+        while i > 0:
+            if abs(self.history[0][0] - self.history[i][0]) < self.w and abs(self.history[0][1] - self.history[i][1]) < self.h and self.length > 2:
+                return True
+            i -= 1
+
+    def update(self):
+        i = self.length - 1
+        while i > 0:
+            self.history[i] = copy.deepcopy(self.history[i-1])
+            i -= 1
+        self.history[0][0] += self.x_dir*scale
+        self.history[0][1] += self.y_dir*scale
+
+
+# ----------- Food Class --------------
+class Food:
+    def new_location(self):
+        global food_x, food_y
+        food_x = random.randrange(1, width/scale-1)*scale
+        food_y = random.randrange(1, height/scale-1)*scale
+
+    def show(self):
+        pygame.draw.rect(display, food_colour, (food_x, food_y, scale, scale))
+
+
+def show_score():
+    font = pygame.font.SysFont("Copperplate Gothic Bold", 20)
+    text = font.render("Score: " + str(score), True, snake_colour)
+    display.blit(text, (scale, scale))
